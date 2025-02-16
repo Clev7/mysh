2db76f9b5b9c5c238a600a57f9450cf3242eaa08
Yousef Abdelsalam <yousefeid707@gmail.com>
2025-02-15 21:02:16 -0500

    implemented movetodir, whereami, and history

--- mysh.c
+++ mysh.c
@@ -14,21 +14,21 @@
 
 /*
     Commands to implement:
-    1. movetodir <path>
-    2. whereami
-    3. history [-c]
-    4. byebye
-    5. replay <number>
-    6. start <program> [parameters]
-    7. background <program> [parameters]
-    8. dalek <PID>
+    1. movetodir <path> (nargs: 1)
+    2. whereami (nargs: 0)
+    3. history [-c] (nargs: 0, 1)
+    4. byebye (nargs: 0)
+    5. replay <number> (nargs: 1)
+    6. start <program> [parameters] (nargs: 1+)
+    7. background <program> [parameters] (nargs: 1+)
+    8. dalek <PID> (nargs: 1)
 
     Bonuses:
-    9. repeat <n> <command> _
-    10. dalekall
+    9. repeat <n> <command> _ (nargs: 2+)
+    10. dalekall (nargs: 0)
 */
 
-typedef struct {
+typedef struct rl_result {
     char *buffer;
     size_t bytes_read;
 } rl_result;
@@ -104,63 +104,121 @@ rl_result *readline(FILE *stream, size_t *buf_size) {
     return res;
 }
 
+typedef struct parse_command_result {
+    vector_str *argv;
+    char *command;
+} parse_command_result;
+
+parse_command_result *parse_command(char *buffer) {
+    parse_command_result *res = (parse_command_result *) malloc(sizeof(parse_command_result));
+    if (!res) {
+        fprintf(stderr, "malloc failed\n");
+        return NULL;
+    }
+
+    res->argv = init_vect_str();
+
+    if (!res->argv) {
+        fprintf(stderr, "malloc failed\n");
+        return NULL;
+    }
+
+    res->command = strtok(buffer, " ");
+
+    if (!res->command) {
+        return res;
+    }
+
+    vect_str_push(res->argv, res->command);
+
+    while (true) {
+        char *token = strtok(NULL, " ");
+        if (!token) {
+            break;
+        }
+
+        vect_str_push(res->argv, token);
+    }
+
+    return res;
+}
+
+void move_to_dir(vector_str *argv, char *cwd) {
+    // Don't forget the command itself
+    if (argv->size != 2) {
+        fprintf(stderr, "Usage: movetodir <filepath>\n");
+        return;
+    }
+
+    DIR *dir = opendir(argv->vector[1]);
+
+    if (dir) {
+        strcpy(cwd, argv->vector[1]);
+    } else if (errno == ENOENT) {
+        fprintf(stderr, "movetodir: %s\n", strerror(errno));
+    } else {
+        fprintf(stderr, "movetodir: an unknown error occurred\n");
+        // fprintf(stderr, "movetodir: %s\n", strerror(errno));
+    }
+}
+
+
+void whereami(vector_str *argv, const char *cwd) {
+    if (argv->size != 1) {
+        fprintf(stderr, "Usage: whereami");
+        return;
+    }
+
+    printf("%s\n", cwd);
+}
+
+void history_command(vector_str *argv, vector_str *history) {
+    if (argv->size > 2) {
+        fprintf(stderr, "Usage: history [-c]\n");
+    } else if (argv->size == 2) {
+        if (!strcmp(argv->vector[1], "-c")) {
+            vect_str_clear(history);
+        } else {
+            fprintf(stderr, "Usage: history [-c]");
+        }
+    } else {
+        for (size_t i = 0; i < history->size; i++) {
+            printf("%ld %s\n", i + 1, history->vector[i]);
+        }
+    }
+}
+
 int main(void) {
     char *cwd = getcwd(NULL, 0);
 
-    vector_str *v = init_vect_str();
+    vector_str *history = init_vect_str();
 
     while (true) {
         printf("# ");
 
         size_t *buf_size = (size_t *) calloc(1, sizeof(size_t));
         rl_result *res = readline(stdin, buf_size);
-        vect_str_push(v, res->buffer);
+        vect_str_push(history, res->buffer);
 
         if (!res || !res->buffer) {
             printf("Null res pointer\n");
             continue;
         }
 
-        const char *command = strtok(res->buffer, " ");
-        char *args = strtok(NULL, " ");
+        parse_command_result *pcr = parse_command(res->buffer);
+        const char *command = pcr->command;
+        vector_str *argv = pcr->argv;
 
         if (!strcmp(command, "movetodir")) {
-            DIR *dir = opendir(args);
-
-            // file exists
-            if (dir) {
-                cwd = args;
-                closedir(dir);
-            } else if (errno == ENOENT) {
-                printf("Not a valid folder path\n");
-            } else {
-                printf("movetodir failed for some unknown reason\n");
-            }
+            move_to_dir(argv, cwd);
         } else if (!strcmp(command, "whereami")) {
-            if (args != NULL) {
-                printf("whereami: invalid usage\n");
-                printf("usage: whereami\n");
-                continue;
-            }
-
-            printf("%s\n", cwd);
+            whereami(argv, cwd);
         } else if (!strcmp(command, "history")) {
-            if (args == NULL) {
-                for (size_t i = 0; i < v->size; i++) {
-                    printf("%ld %s\n", i + 1, v->vector[i]);
-                }
-            } else if (!strcmp(args, "-c")) {
-                vect_str_clear(v);
-            } else {
-                printf("history: invalid option(s)\n");
-                printf("history: usage: history [-c]\n");
-            }
-
+            history_command(argv, history);
         } else if (!strcmp(command, "byebye")) {
             printf("exiting...\n");
             exit(0);
         } else if (!strcmp(command, "replay")) {
-
         } else if (!strcmp(command, "start")) {
 
         } else if (!strcmp(command, "background")) {
@@ -175,5 +233,7 @@ int main(void) {
             printf("%s: command not found\n", command);
         }
 
+        // Clean up after yourself
+        vect_str_clear(argv);
     }
 }
